8394d7f42546b3654a033bc6c8d874bde1c6e750
Andy Steele <andy@spacemeld.com>
2014-05-10 19:04:06 -0400

    Updated main page

--- www/index.php
+++ www/index.php
@@ -9,42 +9,45 @@
     <link href="assets/css/main.css" rel="stylesheet">
   </head>
   <body>
-    <div class="container">
-      <nav class="navbar navbar-default" role="navigation">
-        <div class="container-fluid">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">(LOGO) Cell Miner</a>
+    <div class="bg-miners">
+      <div class="container">
+        <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-phone colored-icon"></span> Cell Miner</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Find a Plan!</a></li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#">Login</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+        <div class="row bg-spacing">
+          <div class="col-xs-8 col-xs-offset-2">
+            <h2>Find your cellphone plan without fuss...</h2>
+            <h3>Just enter your info below and we'll send out the miners to get you the best plans!</h3>
           </div>
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Find a Plan!</a></li>
-            </ul>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Login</a></li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-      </nav>
-      <div class="row">
-        <div class="jumbotron">
-          <h2>Find your cellphone <span class="glyphicon glyphicon-phone coroled-icon"></span> plan without fuss...</h2>
-          <h3>Just enter your info below and we'll send out the miners to get you the best plans!</h3>
         </div>
       </div>
-      <div class="row">
-        <div class="col-xs-8 col-xs-offset-2 well">
-          <p>Search Now</p>
+    </div>
+    <div class="container">
+      <div class="row search">
+        <div class="col-xs-8 col-xs-offset-2">
           <form >
             <div class="form-group input-group input-group-lg">
-                  <input type="text" name="id" placeholder="Enter the custumer id number" class="form-control"/>
+                  <input type="text" name="id" placeholder="Enter your customer id here" class="form-control"/>
                   <span class="input-group-btn">
                      <button type="submit" name="search" value="search" class="btn btn-default">Search Now  <span class="glyphicon glyphicon-search"></span></button>
                   </span>
@@ -56,5 +59,6 @@
     </div>
     <script src="assets/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/main.js"></script>
   </body>
 </html>
