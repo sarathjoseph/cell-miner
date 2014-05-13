d706ed885232cb3eb40a1af7a6d30a0e2c51fa5c
Andy Steele <andy@spacemeld.com>
2014-05-13 09:59:54 -0400

    Updated miner
    Thanks to Sarath

--- files/miner/app.js
+++ files/miner/app.js
@@ -1,30 +1,66 @@
-
 var express = require('express');
 var url = require('url');
 var app = express();
 var fs = require("fs");
 var http = require('http');
 var fs = require("fs");
-var connect=require("connect");
-var server = http.createServer(app).listen(9200);
+var connect = require("connect");
+var server = http.createServer(app).listen(8000);
 app.use(express.static(__dirname + '/public'));
+
 app.use(function (req, res, next) {
-        res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-	res.setHeader("Access-Control-Allow-Origin", "http://localhost");
-        next();
-    });
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost");
+    next();
+});
+
+var request = require("request");
+
+var ip1 = 'http://localhost/cellminer/';
+var ip2 = 'http://localhost/cellminer/';
+var ip3 = 'http://localhost/cellminer/';
+var ip4 = 'http://localhost/cellminer/';
+
+
 
 
 app.get('/mine', function (req, res) {
 
+	
+	var plans = [];
+    var id = req.param("id");
+    var phone = req.param("phone");
+    var mine_path = '/getPlan.php?id='+id+"&phone="+phone;
 
-res.send("Id sent is " + req.param("id")+"phone flag is "+req.param("phone"));
+    request.get(ip1 + mine_path, function (error, response, body) {
 
+        plans.push(body);
 
+        request.get(ip2 + mine_path, function (error, response, body) {
+
+            plans.push(body);
+
+            request.get(ip3 + mine_path, function (error, response, body) {
+
+                plans.push(body);
+
+                request.get(ip4 + mine_path, function (error, response, body) {
+
+                    plans.push(body);
+                    res.send(JSON.stringify(plans));
+
+                });
+               
+
+            });
+           
+
+        });
+
+    });
 
 
 });
-
